a0ae7986952e58c4ca94a5af28d976f2c7d5a4ca
Md. Mazaharul Islam Bhuiyan <mazharrayhan3737@gmail.com>
2020-10-17 23:10:43 +0600

    delete item from the list

--- todo-list/src/components/Todolist.vue
+++ todo-list/src/components/Todolist.vue
@@ -2,7 +2,14 @@
   <div>
     <input type="text" class="todo-input" placeholder="Add todo Here..." v-model="newTodo" @keyup.enter="addTodo" />
 
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">{{todo.title}}</div>
+    <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item">
+      <div>
+        {{todo.title}}
+      </div>
+
+      <div class="remove-item" @click="removeTodo(index)">&times;</div>
+    </div>
+    
   </div>
 </template>
 
@@ -18,7 +25,7 @@ export default {
   },
   methods:{
     addTodo(){
-      if(this.newTodo.trim()==0){
+      if(this.newTodo.trim().length==0){
         return;
       }
 
@@ -29,7 +36,12 @@ export default {
 
       this.newTodo = '';
       this.idForTodo++;
+    },
+
+    removeTodo(index){
+      this.todos.splice(index,1);
     }
+
   }
 };
 </script>
@@ -45,5 +57,22 @@ export default {
 
   &:focus{
     outline: 0;
+  }
+
+  .todo-item{
+    margin-bottom: 12px;
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    padding-left: 150px;
+    font-size: 20px;
+  }
+
+  .remove-item{
+    cursor: pointer;
+    margin-right: 150px;
+    &:hover{
+      color:black;
+    }
   }
 </style>
